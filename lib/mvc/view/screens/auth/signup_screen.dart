278a059587a8adffa9a93ef25d9f52a79c3ce2fc
mediazytechnologies <mediazytech@gmail.com>
2024-06-20 17:41:49 +0530

    dfd

--- lib/mvc/view/screens/auth/signup_screen.dart
+++ lib/mvc/view/screens/auth/signup_screen.dart
@@ -7,7 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:mediezy_medical/mvc/controller/controller/auth/signup/signup_controller.dart';
 import 'package:mediezy_medical/mvc/view/services/app_colors.dart';
 import 'package:mediezy_medical/mvc/view/screens/auth/login_screen.dart';
